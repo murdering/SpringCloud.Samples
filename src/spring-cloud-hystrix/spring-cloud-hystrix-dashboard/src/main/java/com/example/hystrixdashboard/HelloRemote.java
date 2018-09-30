a3f49d544723988404458b30f1306fa3803eb1e1
Leodai <leo_x_dai@manulife.com>
2018-09-30 17:27:18 +0800

    Hystrix Dashboard

---
+++ src/spring-cloud-hystrix/spring-cloud-hystrix-dashboard/src/main/java/com/example/hystrixdashboard/HelloRemote.java
@@ -0,0 +1,12 @@
+package com.example.hystrixdashboard;
+
+import com.example.hystrixdashboard.hystrix.HelloRemoteHystrix;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
---
+++ src/spring-cloud-hystrix/spring-cloud-hystrix-dashboard/src/main/java/com/example/hystrixdashboard/HystrixdashboardApplication.java
@@ -0,0 +1,20 @@
+package com.example.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class HystrixdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixdashboardApplication.class, args);
+	}
+}
