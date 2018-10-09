c820e5f42f5788a94e80f1aeac11b7f209c5a3e2
PHBDOMAIN\leodai <murdering@live.cn>
2018-10-09 16:21:23 +0800

    Hystrix Dashboard 和 Turbine

---
+++ src/spring-cloud-hystrix/spring-cloud-consumer-node1/src/main/java/com/example/node1/HelloRemote.java
@@ -0,0 +1,12 @@
+package com.example.node1;
+
+import com.example.node1.hystrix.HelloRemoteHystrix;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
